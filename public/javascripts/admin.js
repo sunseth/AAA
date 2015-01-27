5e80d6c009b5dfbdd6211f0d1df339d52d15a58c
Seth Sun <sunseth@berkeley.edu>
2015-01-27 15:29:58 -0800

    working ajax add, attempting to abstract into function

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,9 +1,4 @@
 $(document).ready(function(){
-	$('.event-el-name:last').css('border-bottom', 'none');
-	$('.family-el-name:last').css('border-bottom', 'none');
-	$('.committee-el-name:last').css('border-bottom', 'none');
-	$('.cabinet-el-name:last').css('border-bottom', 'none');
-	$('.media-el-name:last').css('border-bottom', 'none');
 
 	$('.delete-button').click(function(){
 		var delReq = JSON.stringify({
@@ -37,14 +32,17 @@ $(document).ready(function(){
 			processData: false,
 			success: function(data){
 				var dataObj = JSON.parse(data);
-				// $.ajax({
-				// 	url: '/handlebars/event-list-item.handlebars',
-				// 	dataType: 'text',
-				// 	success:
-				// });
-				$('#event-list').append('<div>''</div>');
+				$.ajax({
+					url: '/handlebars/event-list-item.handlebars',
+					dataType: 'text',
+					success: function(hbs){
+						var template = Handlebars.compile(hbs);
+						var listElement = template(dataObj);
+						$('#event-list').append(listElement);
+					}
+				});
 				$('#event-submit div input').val('');
-				$('event-submit div textarea').val('');
+				$('#event-submit div textarea').val('');
 			},
 			error: function(err){
 				console.error(err);
@@ -52,4 +50,58 @@ $(document).ready(function(){
 		});
 		evt.preventDefault();
 	});
+
+	$('#family-submit').on('submit', function(evt){
+		var formURL = $(this).attr('action');
+		var formData = new FormData($('form')[1]);
+		$.ajax({
+			url: formURL,
+			data: formData,
+			type: 'POST',
+			contentType: false,
+			processData: false,
+			success: function(data){
+				var dataObj = JSON.parse(data);
+				$.ajax({
+					url: '/handlebars/family-list-item.handlebars',
+					dataType: 'text',
+					success: function(hbs){
+						var template = Handlebars.compile(hbs);
+						var listElement = template(dataObj);
+						$('#family-list').append(listElement);
+					}
+				});
+				$('#family-submit div input').val('');
+				$('#family-submit div textarea').val('');
+			},
+			error: function(err){
+				console.error(err);
+			}
+		});
+		evt.preventDefault();
+	});
+
+	// $('.submit-post').on('submit', function(event){
+	// 	var id = $(this).attr('id');
+	// 	var url = $(this).attr('action');
+	// 	if (id=='event-submit'){
+	// 		ajaxAdd(url, 0, '/handlebars/event-list-item.handlebars', $('#family-list'), 
+	// 			$('#family-submit div input'), $('#family-submit div textarea'));
+	// 	} else if (id=='family-submit'){
+
+	// 	} else if (id=='committee-submit'){
+
+	// 	} else if (id=='cabinet-submit'){
+
+	// 	} else if (id=='media-submit'){
+
+	// 	} else {
+	// 		console.error('Invalid form');
+	// 	}
+	// 	event.preventDefault();
+	// });
+
+	// function ajaxAdd(url, formNum, handlebarsPath, addTo, inputClean, textareaClean){
+
+	// }
 });
